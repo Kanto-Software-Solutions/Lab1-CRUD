20b953c13f9922de87d6bef440b6ddb7366331c2
leoleguizamon <anlleguizamongu@unal.edu.co>
2023-08-31 21:05:02 -0500

    =

--- src/router.js
+++ src/router.js
@@ -3,69 +3,69 @@ const router = express.Router();
 
 module.exports = router;
 
-app.get('/departamento', (req, res) => {
+router.get('/departamento', (req, res) => {
     res.render('departamento');
 });
-app.get('/departamento1', (req, res) => {
+router.get('/departamento1', (req, res) => {
     res.render('departamento1');
 });
-app.get('/departamento2', (req, res) => {
+router.get('/departamento2', (req, res) => {
     res.render('departamento2');
 });
-app.get('/departamento3', (req, res) => {
+router.get('/departamento3', (req, res) => {
     res.render('departamento3');
 });
-app.get('/municipio', (req, res) => {
+router.get('/municipio', (req, res) => {
     res.render('municipio');
 });
-app.get('/municipio1', (req, res) => {
+router.get('/municipio1', (req, res) => {
     res.render('municipio1');
 });
-app.get('/municipio2', (req, res) => {
+router.get('/municipio2', (req, res) => {
     res.render('municipio2');
 });
-app.get('/municipio3', (req, res) => {
+router.get('/municipio3', (req, res) => {
     res.render('municipio3');
 });
-app.get('/persona', (req, res) => {
+router.get('/persona', (req, res) => {
     res.render('persona');
 });
-app.get('/persona1', (req, res) => {
+router.get('/persona1', (req, res) => {
     res.render('persona1');
 });
-app.get('/persona2', (req, res) => {
+router.get('/persona2', (req, res) => {
     res.render('persona2');
 });
-app.get('/persona3', (req, res) => {
+router.get('/persona3', (req, res) => {
     res.render('persona3');
 });
-app.get('/propietario', (req, res) => {
+router.get('/propietario', (req, res) => {
     res.render('propietario');
 });
-app.get('/propietario1', (req, res) => {
+router.get('/propietario1', (req, res) => {
     res.render('propietario1');
 });
-app.get('/propietario2', (req, res) => {
+router.get('/propietario2', (req, res) => {
     res.render('propietario2');
 });
-app.get('/propietario3', (req, res) => {
+router.get('/propietario3', (req, res) => {
     res.render('propietario3');
 });
-app.get('/vivienda', (req, res) => {
+router.get('/vivienda', (req, res) => {
     res.render('vivienda');
 });
-app.get('/vivienda1', (req, res) => {
+router.get('/vivienda1', (req, res) => {
     res.render('vivienda1');
 });
-app.get('/vivienda2', (req, res) => {
+router.get('/vivienda2', (req, res) => {
     res.render('vivienda2');
 });
-app.get('/vivienda3', (req, res) => {
+router.get('/vivienda3', (req, res) => {
     res.render('vivienda3');
 });
-app.get('/taskController', (req, res) => {
+router.get('/taskController', (req, res) => {
     res.render('taskController');
 });
-app.get('/index', (req, res) => {
+router.get('/index', (req, res) => {
     res.render('index');
 });
