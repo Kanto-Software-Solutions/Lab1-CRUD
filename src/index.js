922f5090176d0c176a210b676bbb4687d8de5eb0
leoleguizamon <anlleguizamongu@unal.edu.co>
2023-08-31 21:50:56 -0500

    Organizacion proyecto

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 //---------------------------------------------------------------------------Var
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
 const path = require("path");
-const mysql = require("mysql2");
 
 //---------------------------------------------------------------------------Configuracion
 app.set("port", process.env.PORT || 3003);
@@ -14,31 +12,6 @@ app.set("views", path.join(__dirname, "views"));
 app.listen(app.get("port"), () => {
 	console.log("Server on port http://localhost:3003", app.get("port"));
 });
-
-//---------------------------------------------------------------------------DB
-const conexion = mysql.createConnection(
-	{
-		host: "localhost",
-		user: "webuser",
-		password: "1234",
-		port: 3306,
-		database: "lab1",
-	},
-	"single"
-);
-
-conexion.connect((error) => {
-	if (error) {
-		console.error("No se pudo conectar a la BD: " + error);
-		return;
-	}
-	console.log("Conectado a la DB");
-});
-
-module.exports = conexion;
-//---------------------------------------------------------------------------Middlewares
-app.use(morgan("dev"));
-app.use(express.json());
 
 //---------------------------------------------------------------------------Router
 app.use("/", require("./router"));
