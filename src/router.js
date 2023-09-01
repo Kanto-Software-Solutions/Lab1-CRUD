ec506ca3ce231d25843ccc331ca37abcc2097540
leoleguizamon <anlleguizamongu@unal.edu.co>
2023-08-31 20:47:43 -0500

    Creacion de router y eliminacion de task

--- src/router.js
+++ src/router.js
@@ -1,53 +1,8 @@
 const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const path = require('path');
-const mysql =require("mysql2");
-const taskController = require('./controllers/taskController.js');
+const router = express.Router();
 
-//Configuraciones
-app.set('port', process.env.PORT || 3003); //Puerto del servidor por defecto def por el SO
-app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'views'));
+module.exports = router;
 
-//Conexion con el servidor
-app.listen(app.get('port'),()=>{
-	console.log('Server on port http://localhost:3003', app.get('port'));
-});
-
-//Conexion bd
-const conexion=mysql.createConnection( {
-    host: 'localhost',
-    user: 'webuser',
-    password: '1234',
-    port: 3306,
-    database: 'lab1'
-}, 'single');
-conexion.connect((error) =>{
-	if(error){
-		console.error('No se pudo conectar a la BD: '+error);
-		return
-	}
-	console.log('Conectado a la DB');
-});
-
-//Middlewares
-app.use(morgan('dev'));
-app.use(express.json());
-
-
-module.exports = conexion; //Exportar para CRUD
-
-app.use(express.urlencoded({extended:false}));
-
-
-
-
-//Archivos estaticos
-app.use(express.static(__dirname + '/public'));
-
-
-//Rutas
 app.get('/departamento', (req, res) => {
     res.render('departamento');
 });
@@ -111,16 +66,6 @@ app.get('/vivienda3', (req, res) => {
 app.get('/taskController', (req, res) => {
     res.render('taskController');
 });
-
 app.get('/index', (req, res) => {
     res.render('index');
 });
-
-app.post('/departamento/add', taskController.save);
-app.post('/municipio/add', taskController.save);
-app.post('/persona/add', taskController.save);
-app.post('/propietario/add', taskController.save);
-app.post('/vivienda/add', taskController.save);
-
-
-
