04b9ad9e1865cb75449eeda596808aad4d440b7c
leoleguizamon <anlleguizamongu@unal.edu.co>
2023-08-31 22:20:56 -0500

    correcion index

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-//---------------------------------------------------------------------------Var
+//---------------------------------------------------------------------------Variables
 const express = require("express");
-const app = express();
 const path = require("path");
+const app = express();
 
 //---------------------------------------------------------------------------Configuracion
-app.set("port", process.env.PORT || 3003);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");						//motor de vistas
+app.set("views", path.join(__dirname, "/views"));	//Cambia la ubicacion de las vistas
+app.set("port", process.env.PORT || 3003);			//Configura puerto a usar
+
+app.use(express.static(__dirname + "/public"));		//Carpeta de recursos publicos
+app.use(express.urlencoded({ extended: false }));	//???
+
 
 //---------------------------------------------------------------------------Puerto
 app.listen(app.get("port"), () => {
-	console.log("Server on port http://localhost:3003", app.get("port"));
+	console.log("Server on port http://localhost:"+app.get("port"));
 });
 
 //---------------------------------------------------------------------------Router
 app.use("/", require("./router"));
-
-app.use(express.urlencoded({ extended: false }));
-
-//Archivos estaticos
-app.use(express.static(__dirname + "/public"));
